9124098a26a1344dc468c38765525e8141b4e2c5
suraj basnal <surajsnghbasnal@gmail.com>
2024-12-04 21:58:29 +0530

    Initializing Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "./assets/Crypto Currency 3D Animated Icon.gif";
+
+const App = () => {
+  return (
+    <div className='card'>
+      <img src={logo} width='60' alt='Logo'/>
+    </div>
+  )
+}
+
+export default App
